808c6236a921dea20d2566d0253689732ec3abfd
guzhongren <guzhongren@live.cn>
2018-02-09 13:00:31 +0800

    custorm Modules

---
+++ src/@types/index.d.ts
@@ -0,0 +1,8 @@
+declare module "customModule" {
+	const CustomModule: any;
+	export = CustomModule;
+}
+decare module 'path/customModule1' {
+	const CustomModule1;
+	export = CustomModule1;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/// <reference path="./@types/index.d.ts">
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 import EsriMapExt from './EsriMapExt';
